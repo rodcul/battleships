f2e0a55bd104970330079b25f03a929097294dd5
Rodney Cullen <rodcul@gmail.com>
2015-05-05 23:50:13 +0100

    [US04] Constrain ships to the board

--- lib/board.rb
+++ lib/board.rb
@@ -12,39 +12,37 @@ def mapper(location)
   alphabet = ('a'..'z').to_a
   x, y = location.chars
   @array = alphabet.index(x) , y.to_i-1
+end
 
-
+def check_on_board (x,y)
+  grid = self.places.count
+  fail 'Outside board' if x > grid ||  y > grid || x < 0 || y < 0
+  true
 end
 
 def place_ship(location,input_ship,direction)
+
   ship = Ship.new
   size = ship.ships[input_ship]
-  x,y = mapper location
+  x, y = mapper location
 
-  if direction == :right
+  check_on_board(x,y)
+  direction == 'vertical' ? check_on_board(x+size,y) : check_on_board(x,y+size)
+
+
+  if direction == 'horizontal'
     i = 0
     while i < size
       places[x][y+i] = 's'
       i += 1
     end
-  elsif direction == :down
+  else direction == 'vertical'
     i = 0
     while i < size
       places[x+i][y] = 's'
       i += 1
     end
-  elsif direction == :left
-    i = 0
-    while i < size
-      places[x][y-i] = 's'
-      i += 1
-    end
-  else direction == :up
-    i = 0
-    while i < size
-      places[x-i][y] = 's'
-      i += 1
-    end
+
   end
 
 end
@@ -54,9 +52,10 @@ def lookup location
   places[x][y]
 end
 
+
 def print
   places.each do |r|
-  puts r.map { |p| p }.join(" ")
+  puts r.map { |p| p ? p : '·' }.join(" ")
 end
 end
 end
--- spec/features/feature_battleship_spec.rb
+++ spec/features/feature_battleship_spec.rb
@@ -11,10 +11,28 @@ feature Board do
     scenario 'allows placing ship in a location' do
 
       board = Board.new
-      board.place_ship('a3',:destroyer,:down)
+      board.place_ship('a3',:destroyer,'vertical')
       expect(board.lookup('a3')).to eq 's'
       expect(board.lookup('b3')).to eq 's'
 
+    end
+
+    scenario 'disallows placing ship outside of board' do
+      board = Board.new 3
+      expect{ board.place_ship('d4',:destroyer,'vertical') }.to raise_error 'Outside board'
+    end
+
+    scenario 'disallows placing ship that extends outside of board' do
+      board = Board.new 3
+      expect{ board.place_ship('c3',:destroyer,'vertical') }.to raise_error 'Outside board'
+      expect( board.lookup("c3")).to be nil
+    end
+
+    xscenario 'disallows placing ship on top of another' do
+      board = Board.new 3
+      board.place_ship('a1',:cruiser,'vertical')
+      expect{ board.place_ship('c1',:destroyer,'horizontal') }.to raise_error 'Overlap'
+      expect( board.lookup("c2")).to be nil
     end
 
 
